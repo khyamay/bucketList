31c1b4f6f4535bd5de85f8facc8df1d468ca061c
Khemraj Thapa <khyamay@gmail.com>
2014-07-06 23:33:50 +0100

    added more templates and functionality

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,5 +1,5 @@
 angular.module('bucketList.controllers', [])
-	.controller('SingUpCtrl', ['$scope', '$rootScope', '$firebaseAuth', '$window', function ($scope, $rootScope, $firebaseAuth, $window){
+	.controller('SignUpCtrl', ['$scope', '$rootScope', '$firebaseAuth', '$window', function ($scope, $rootScope, $firebaseAuth, $window){
 		$scope.user = {
 			email: "",
 			password: ""
@@ -35,4 +35,44 @@ angular.module('bucketList.controllers', [])
 				}
 			}
 		});
-	}]);
+	}])
+	.controller('SignInCtrl', ['$scope', '$rootScope', 'firebaseAuth', '$window', function ($scope, $rootScope, $firebaseAuth, $window){
+		$rootScope.checkSession();
+		$scope.user = {
+			email: '',
+			password: ''
+		};
+
+		$scope.validateUser = function (){
+			$rootScope.show('Please wait... Authenticating');
+			var email = this.user.email;
+			var password = this.user.password;
+			if (!email || !password){
+				$rootScope.notify('Please enter valid credentials');
+				return false;
+			}
+			$rootScope.auth.$login('password', {
+				email: email,
+				password: password
+			})
+			.then(function (user){
+				$rootScope.hide();
+				$rootScope.userEmail = user.email;
+				$window.location.href = ('#/bucket/list');
+			}, function (error){
+				$rootScope.hide();
+				if (error.code == 'INVALID_EMAIL'){
+					$rootScope.notify('Invalid Email Address');
+				}
+				else if (error.code == 'INVALID_PASSWORD'){
+					$rootScope.notify('Invalid Password');
+				}
+				else if (error.code == 'INVALID_USER'){
+					$rootScope.notify('Invalid User');
+				}
+				else {
+					$rootScope.notify('Opps something went wrong. Please try it again later');
+				}
+			});
+		}
+	}])
